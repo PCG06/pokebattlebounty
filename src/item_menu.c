6141ec4ed12e0ebc517b96e723e43847157761a8
PCG06 <pcggaming06@gmail.com>
2026-05-31 23:48:13 +0530

    Fix: Aligned bag pocket indicators

--- src/item_menu.c
+++ src/item_menu.c
@@ -789,6 +789,7 @@ static bool8 SetupBagMenu(void)
         CopyPocketNameToWindow(0);
         DrawPocketIndicatorSquare(0, FALSE);
         DrawPocketIndicatorSquare(1, FALSE);
+        DrawPocketIndicatorSquare(6, FALSE);
         DrawPocketIndicatorSquare(7, FALSE);
         DrawPocketIndicatorSquare(gBagPosition.pocket, TRUE);
         gMain.state++;
@@ -1488,9 +1489,9 @@ static void DrawItemListBgRow(u8 y)
 static void DrawPocketIndicatorSquare(u8 x, bool8 isCurrentPocket)
 {
     if (!isCurrentPocket)
-        FillBgTilemapBufferRect_Palette0(2, 0x1017, x + 3, 3, 1, 1);
+        FillBgTilemapBufferRect_Palette0(2, 0x1017, x + 4, 3, 1, 1);
     else
-        FillBgTilemapBufferRect_Palette0(2, 0x102B, x + 3, 3, 1, 1);
+        FillBgTilemapBufferRect_Palette0(2, 0x102B, x + 4, 3, 1, 1);
     ScheduleBgCopyTilemapToVram(2);
 }
 
