17ffdb6591c4c2587e618bd230929be92b849452
PCG06 <pcggaming06@gmail.com>
2026-06-16 19:27:09 +0530

    Feat: Implement Gen9 TMs

--- test/text.c
+++ test/text.c
@@ -228,13 +228,14 @@ TEST("Item names fit on Shop Screen")
 TEST("Item descriptions fit on Bag and Shop Screen")
 {
     u32 i;
-    const u32 fontId = FONT_NORMAL, widthPx = 102;
+    const u32 widthPx = 102;
+    u32 fontId = FONT_NORMAL;
     enum Item item = ITEM_NONE;
     for (i = 1; i < ITEMS_COUNT; i++)
     {
-        PARAMETRIZE_LABEL("%S", gItemsInfo[i].description) { item = i; }
+        PARAMETRIZE_LABEL("%S", GetItemDescription(i)) { item = i; fontId = (GetItemPocket(i) == POCKET_TM_HM) ? FONT_NARROW : fontId; }
     }
-    EXPECT_LE(GetStringWidth(fontId, gItemsInfo[item].description, 0), widthPx);
+    EXPECT_LE(GetStringWidth(fontId, GetItemDescription(item), 0), widthPx);
 }
 
 TEST("Species names fit on Battle Screen HP box")
