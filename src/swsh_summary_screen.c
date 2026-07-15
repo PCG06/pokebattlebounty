2c4b8f0f995107a54bc3d9ac7e3e2ee94e2fdff2
PCG06 <pcggaming06@gmail.com>
2026-07-15 18:22:49 +0530

    fix(summry screen): stat editor prompt overlapping with moves screen

--- src/swsh_summary_screen.c
+++ src/swsh_summary_screen.c
@@ -810,7 +810,7 @@ static const struct WindowTemplate sPageMovesTemplate[] = // This is used for bo
         .width = 18,
         .height = 5,
         .paletteNum = 2,
-        .baseBlock = 401,
+        .baseBlock = 411,
     },
 };
 static const struct WindowTemplate sPageMemoTemplate[] =
@@ -2863,8 +2863,7 @@ static void DrawNextSkillsButtonPrompt(u8 mode)
     if (ShouldShowStatEditor())
     {
         FillWindowPixelBuffer(PSS_LABEL_WINDOW_PROMPT_STAT_EDITOR, PIXEL_FILL(0));
-        PrintButtonIcon(PSS_LABEL_WINDOW_PROMPT_STAT_EDITOR, BUTTON_START, 0, 4);
-        PrintTextOnWindowWithFont(PSS_LABEL_WINDOW_PROMPT_STAT_EDITOR, sText_StatEditor, 27, 0, 0, 1, FONT_SMALL);
+        PrintRightAlignedPrompt(PSS_LABEL_WINDOW_PROMPT_STAT_EDITOR, BUTTON_START, sText_StatEditor, 78, 1);
     }
     ScheduleBgCopyTilemapToVram(0);
 }
